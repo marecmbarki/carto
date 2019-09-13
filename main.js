bbb93865b183a0dcf7139f6240af2a1c2f9d6d74
marecmabrki <49040784+marecmabrki@users.noreply.github.com>
2019-09-13 14:59:57 +0200

    13/09

--- main.js
+++ main.js
@@ -1,46 +1,13 @@
-// Prend en paramètres l'URL cible et la fonction callback appelée en cas de succès
-function ajaxGet(url, callback) {
-    var req = new XMLHttpRequest();
-    req.open("GET", url);
-    req.addEventListener("load", function () {
-        if (req.status >= 200 && req.status < 400) {
-            // Appelle la fonction callback en lui passant la réponse de la requête
-            callback(req.responseText);
-        } else {
-            console.error(req.status + " " + req.statusText + " " + url);
-        }
-    });
-    req.addEventListener("error", function () {
-        console.error("Erreur réseau avec l'URL " + url);
-    });
-    req.send(null);
+const mapCont = new Map();
+
+mapCont.load();
+const nameForm = document.getElementById("form_name").value;
+const firstNameForm = document.getElementById("form_first_name").value;
+
+if (nameForm != "" && firstNameForm != "") {
+    console.log(`Ton nom est ${nameForm} et ton prénom est ${firstNameForm}`);
+    document.querySelector('input[type="submit"]').removeAttribute("disabled");
 }
 
-ajaxGet("https://api.jcdecaux.com/vls/v3/stations?contract=toyama&apiKey=76b2d676841c793fb1d815d95caf51aba293ce35", function (response) {  
-    var velos = JSON.parse(response);
-   
-    function dispInfos(velo) {
-        document.getElementById("address").textContent = velo.address;
-        console.log(velo);
-    }
-
-    velos.forEach((velo) => {
-        const x = velo.position.latitude;
-        const y = velo.position.longitude;
-        const marker = L.marker([x, y]).addTo(map);
-        marker.addEventListener("click", () => dispInfos(velo));
-    });
-});
-
-var map = L.map('map').setView([51.505, 137.2183531], 13);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox.streets',
-    accessToken: 'your.mapbox.access.token'
-}).addTo(map);
+localStorage.setItem("test", "bonjour");
+console.log(localStorage.getItem("test"));
---
+++ map.js
@@ -0,0 +1,39 @@
+
+class Map {
+    load() {
+        const ajax = new Ajax();
+    
+        ajax.ajaxGet("https://api.jcdecaux.com/vls/v3/stations?contract=toyama&apiKey=76b2d676841c793fb1d815d95caf51aba293ce35", function (response) {  
+            const velos = JSON.parse(response);
+        
+            function dispInfos(velo) {
+                //let dispForm = document.getElementById("station_details");
+
+                //dispForm.style.display = "block";
+                document.getElementById("address").textContent = velo.address;
+                document.getElementById("places").textContent = velo.mainStands.availabilities.stands;
+                document.getElementById("bikes").textContent = velo.totalStands.availabilities.bikes;
+            }
+
+            velos.forEach((velo) => {
+                const x = velo.position.latitude;
+                const y = velo.position.longitude;
+                const marker = L.marker([x, y]).addTo(map);
+                marker.addEventListener("click", () => dispInfos(velo));
+            });
+        });
+
+        const map = L.map('map').setView([51.505, 137.2183531], 5);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox.streets',
+            accessToken: 'your.mapbox.access.token'
+        }).addTo(map);
+    }
+}
